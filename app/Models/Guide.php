d03e7d345dbea462cbc9a9b2fec0c791e8dc07e3
MamleevMR <muhammad.mamleev.1998@gmail.com>
2025-05-13 17:52:29 +0500

    добавил связь гида и юзера

--- app/Models/Guide.php
+++ app/Models/Guide.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Guide extends Model
@@ -14,4 +16,14 @@ class Guide extends Model
     protected $fillable = [
         'user_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function excursions(): HasMany
+    {
+        return $this->hasMany(Excursion::class, 'guid_id');
+    }
 }
