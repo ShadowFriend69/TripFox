65a42e0832f2e5f626f7d51c02eca2e12c56787a
MamleevMR <muhammad.mamleev.1998@gmail.com>
2025-05-13 18:26:18 +0500

    доработки ресурса для модели категорий

--- app/Filament/Resources/ExcursionCategoryResource.php
+++ app/Filament/Resources/ExcursionCategoryResource.php
@@ -7,29 +7,41 @@ use App\Filament\Resources\ExcursionCategoryResource\RelationManagers;
 use App\Models\ExcursionCategory;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class ExcursionCategoryResource extends Resource
 {
     protected static ?string $model = ExcursionCategory::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-cube-transparent';
+    protected static ?string $navigationLabel = 'Категории экскурсий';
+    protected static ?string $navigationGroup = 'Экскурсии';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('title')
+                    ->label('Название категории')
                     ->required()
-                    ->maxLength(255),
+                    ->maxLength(255)
+                    ->live()
+                    ->afterStateUpdated(function (Set $set, $state) {
+                        $set('slug', Str::slug($state));
+                    }),
                 Forms\Components\TextInput::make('slug')
+                    ->label('Код')
                     ->required()
                     ->maxLength(255),
                 Forms\Components\Toggle::make('isActive')
+                    ->label('Активна')
+                    ->default(true)
                     ->required(),
             ]);
     }
@@ -39,20 +51,26 @@ class ExcursionCategoryResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('title')
+                    ->label('Название категории')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('slug')
+                    ->label('Код')
                     ->searchable(),
                 Tables\Columns\IconColumn::make('isActive')
+                    ->label('Активна')
                     ->boolean(),
                 Tables\Columns\TextColumn::make('created_at')
+                    ->label('Дата создания')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Дата обновления')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('deleted_at')
+                    ->label('Дата удаления')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
