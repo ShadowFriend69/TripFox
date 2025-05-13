012272bbce2af1dcaa66ec413faa0570c7220de1
MamleevMR <muhammad.mamleev.1998@gmail.com>
2025-05-13 17:52:11 +0500

    добавил кнопку войти

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\ExcursionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/', [ExcursionController::class, 'index'])->name('home');
